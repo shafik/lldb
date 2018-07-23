82759017fbd985071e37b850fb1526fd31f75ecf
Shafik Yaghmour <syaghmour@apple.com>
2018-07-23 14:44:54 -0700

    Removing dead code

--- packages/Python/lldbsuite/test/functionalities/data-formatter/data-formatter-stl/libcxx/optional/main.cpp
+++ packages/Python/lldbsuite/test/functionalities/data-formatter/data-formatter-stl/libcxx/optional/main.cpp
@@ -14,18 +14,14 @@ int main()
     optional_int number_engaged = 42 ;
 
     printf( "%d\n", *number_engaged) ;
-                                        //% self.expect("frame variable number_not_engaged", substrs=["Has Value=false",  "{}"])
-                                       //% self.expect("frame variable number_engaged", substrs=["Has Value=true", "Value = 42", "}"])
 
     optional_int_vect numbers{{1,2,3,4}} ;
 
     printf( "%d %d\n", numbers.value()[0], numbers.value()[1] ) ;
-    //% self.expect("frame variable numbers", substrs=["Has Value=true", "Value = size=4", "[0] = 1", "[1] = 2", "[2] = 3", "[3] = 4", "}"])
 
     optional_string ostring = "hello" ;
 
     printf( "%s\n", ostring->c_str() ) ;
-    //% self.expect("frame variable ostring", substrs=["Has Value=true", "Value = \"hello\"", "}"])
 
     return 0; // break here
 }
